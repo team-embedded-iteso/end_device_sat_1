c61cf88ac3a17eef8abba8d528c5e65e12256353
Marcela Rosales <marcelarosalesj@gmail.com>
2019-04-14 14:31:29 -0500

    Add End Device first commit
    - This ED has address 0xF2 and connects to a coordinator.
    
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

---
+++ boards/frdmk64f_frdmcr20a/wireless_examples/ieee_802_15_4/msn_end_device/freertos/kds/debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Repos/Conn_Wireless/EDF/boards/frdmk64f_frdmcr20a/wireless_examples/ieee_802_15_4/msn_end_device/freertos/kds/../../../../../../../middleware/mmcau_2.0.0/asm-cm4-cm7/lib_mmcau.a C:/Repos/Conn_Wireless/EDF/boards/frdmk64f_frdmcr20a/wireless_examples/ieee_802_15_4/msn_end_device/freertos/kds/../../../../../../../middleware/wireless/ieee_802_15_4_5.0.5/mac/lib/802.15.4_mac_06_cM4.a C:/Repos/Conn_Wireless/EDF/boards/frdmk64f_frdmcr20a/wireless_examples/ieee_802_15_4/msn_end_device/freertos/kds/../../../../../../../middleware/wireless/framework_5.0.5/SecLib/lib_crypto_M4.a
+
+LIBS := -lm -lg -lgcc -lnosys
+
